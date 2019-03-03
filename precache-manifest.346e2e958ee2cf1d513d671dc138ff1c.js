69572a9f7b3b0f0941a6ff4f79365b0391a4b508
ant4u <anthonyimontalto@gmail.com>
2019-03-02 21:42:09 -0500

    Updates

---
+++ precache-manifest.346e2e958ee2cf1d513d671dc138ff1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ClickyGame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2c3a1b5a67c5d677a07d",
+    "url": "/ClickyGame/static/js/runtime~main.2c3a1b5a.js"
+  },
+  {
+    "revision": "2e8e1f3e5341ced1e588",
+    "url": "/ClickyGame/static/js/main.2e8e1f3e.chunk.js"
+  },
+  {
+    "revision": "6ddd7de4aeeeb30d60dc",
+    "url": "/ClickyGame/static/js/2.6ddd7de4.chunk.js"
+  },
+  {
+    "revision": "2e8e1f3e5341ced1e588",
+    "url": "/ClickyGame/static/css/main.3b09ef88.chunk.css"
+  },
+  {
+    "revision": "85a0f8222348d67045aca87edfafa1ed",
+    "url": "/ClickyGame/index.html"
+  }
+];
