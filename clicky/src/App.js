52e8848f289eba54af7b1539a64561108772be77
ant4u <anthonyimontalto@gmail.com>
2019-03-03 01:40:44 -0500

    rendered cards

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,34 +1,42 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+// import "./App.css";
 import Wrapper from "./components/Wrapper";
 import GameCard from "./components/GameCard";
-import Title from "./components/Title";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Button from "react-bootstrap/Button";
-import pics from "./cards.json"
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import pics from "./cards.json";
 
 function App() {
- 
-    return (
-      <Wrapper>
-        <Title>Clicky Game</Title>
-        <Jumbotron>
-          <h1>Clicky Game!</h1>
-          <p>
-            Click on an image to earn points, but don't click on any more than once!
-          </p>
-          <GameCard
-            image = {pics[2].image}
-            id = {pics[2].id}
-            />
-          <p>
-            <Button variant="primary">Learn more</Button>
-          </p>
-        </Jumbotron>;
-      </Wrapper>
+  return (
+    <Wrapper>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Clicky Game</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link>Click an image to begin!</Nav.Link>
+        </Nav>
+        <Nav className="mr-auto">
+          <Nav.Link>Score: 0</Nav.Link> |<Nav.Link>Top Score: 0</Nav.Link>
+        </Nav>
+      </Navbar>
+      <Jumbotron>
+        <h1>Clicky Game!</h1>
+        <p>
+          Click on an image to earn points, but don't click on any more than
+          once!
+        </p>
+        {pics.map(item => (
+          <GameCard image={item.image} id={item.id} />
+        ))}
 
-      );
-  
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </Jumbotron>
+      ;
+    </Wrapper>
+  );
 }
 
 export default App;
