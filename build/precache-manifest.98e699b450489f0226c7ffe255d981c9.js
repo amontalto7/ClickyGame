a4082c3344f50d233966a8a2e3cbf3ef8740ca72
ant4u <anthonyimontalto@gmail.com>
2019-06-07 23:12:05 -0400

    updated version, fixed security issues

---
+++ build/precache-manifest.98e699b450489f0226c7ffe255d981c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c3a1b5a67c5d677a07d",
+    "url": "/ClickyGame/static/js/runtime~main.2c3a1b5a.js"
+  },
+  {
+    "revision": "72ae57b3aad30a1cefd6",
+    "url": "/ClickyGame/static/js/main.72ae57b3.chunk.js"
+  },
+  {
+    "revision": "5a8748a232a383595be8",
+    "url": "/ClickyGame/static/js/2.5a8748a2.chunk.js"
+  },
+  {
+    "revision": "72ae57b3aad30a1cefd6",
+    "url": "/ClickyGame/static/css/main.7f0947c0.chunk.css"
+  },
+  {
+    "revision": "f391c8b1a46cd4a9f9c5f892acfeb0f5",
+    "url": "/ClickyGame/index.html"
+  }
+];
